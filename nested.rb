1cd343dbb769785a201e2c4fa3b1111edba933d8
Brian Nicholls <nichol88@gmail.com>
2019-12-11 00:59:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,6 @@ def adding_matz
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
 		 programmer_hash
 end
-
 def changing_alan
 	programmer_hash =
  		{
@@ -107,7 +106,6 @@ def changing_alan
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 		 programmer_hash
 end
-
 def adding_to_dennis
 	programmer_hash =
  		{
@@ -124,5 +122,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
